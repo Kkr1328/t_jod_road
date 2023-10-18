67ed9b2607e76aaa86372ff6bcf6721da9ad0298
Chinnapat.N <kharkkr@gmail.com>
2023-10-18 15:12:22 +0700

    Mock reservation data

---
+++ src/mock/RESERVATION.ts
@@ -0,0 +1,22 @@
+export const MockedReservation = [
+	{
+		first_name: 'somchai01',
+		last_name: 'last01',
+		time_left: '2 mins',
+	},
+	{
+		first_name: 'somchai02',
+		last_name: 'last02',
+		time_left: '10 mins',
+	},
+	{
+		first_name: 'somchai03',
+		last_name: 'last03',
+		time_left: '5 mins',
+	},
+	{
+		first_name: 'somchai04',
+		last_name: 'last04',
+		time_left: '6 mins',
+	},
+];
