683a437934717108c75be9b14787867595b7273c
Palm394 <palm.nichapat@gmail.com>
2023-10-21 22:09:31 +0700

    initialize branch

---
+++ src/app/drive_in/page.tsx
@@ -0,0 +1,11 @@
+import PageTitle from "@/components/common/PageTitle";
+import { NAVBAR_LABEL } from "@/constants/LABEL";
+
+export default function DriveIN() {
+  return (
+    <>
+        <PageTitle title={NAVBAR_LABEL.CUSTOMERS_RESERVATION} />
+        
+    </>
+  )
+}
