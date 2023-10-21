980ffc0929080fa85fc5a600494b0d07088d6b07
Palm394 <palm.nichapat@gmail.com>
2023-10-21 22:34:07 +0700

    Place map layout

--- src/app/drive_in/page.tsx
+++ src/app/drive_in/page.tsx
@@ -1,11 +1,37 @@
+'use client'
+
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+
 import PageTitle from "@/components/common/PageTitle";
 import { NAVBAR_LABEL } from "@/constants/LABEL";
+import Card from '@mui/material/Card';
+
+const center = {
+    lat: 13.738329190226818,
+    lng: 100.52718982270956
+};
 
 export default function DriveIN() {
-  return (
-    <>
-        <PageTitle title={NAVBAR_LABEL.CUSTOMERS_RESERVATION} />
-        
-    </>
-  )
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "<GOOGLE-MAP-KEY>",
+    })
+
+    if (!isLoaded) return 'Loading'
+    return (
+        <div className='h-[70dvh]'>
+            <PageTitle title={NAVBAR_LABEL.CUSTOMERS_RESERVATION} />
+            <Card className='flex w-full h-full my-32 rounded-lg px-32 py-24'>
+                <GoogleMap
+                    options={{ disableDefaultUI: true }}
+                    zoom={16}
+                    center={center}
+                    mapContainerClassName='w-full'
+                >
+                    <Marker
+                        position={center}
+                    />
+                </GoogleMap>
+            </Card>
+        </div>
+    )
 }
