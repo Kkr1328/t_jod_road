ae4d132d225e7ed31da75075ae2e062714cca3b3
Palm394 <palm.nichapat@gmail.com>
2023-11-10 16:04:34 +0700

    fix little bit port

--- src/services/constant.ts
+++ src/services/constant.ts
@@ -1,3 +1,3 @@
-export const USER_SERVICE_URL = "http://localhost:7005"
+export const USER_SERVICE_URL = "http://localhost:7000"
 export const PARKING_SERVICE_URL = "http://localhost:4000"
 export const RESERVATION_SERVICE_URL = "http://localhost:9000"
