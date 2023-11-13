cefd83525f1238c35bc9a7e2dcfb3f723549c32e
Thanawat <oat311@gmail.com>
2023-11-13 20:28:53 +0700

    create review page

---
+++ src/mock/REVIEW.ts
@@ -0,0 +1,18 @@
+export const MockedReview = [
+    {
+        user_id: "John",
+        parking_lot_id: 1,
+        review: "This is a great product!",
+    },
+    {
+        user_id: "Jane",
+        parking_lot_id: 1,
+        review: "I love this product! It's the best I've ever used.",
+    },
+    {
+        user_id: "Peter",
+        parking_lot_id: 1,
+        review: "This product is amazing! It's everything I've ever wanted.",
+    },
+    
+    ];
