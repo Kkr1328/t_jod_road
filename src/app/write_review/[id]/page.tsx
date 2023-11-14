273128042c0422dc1b5484e41342b81714bc3b48
Thanawat <oat311@gmail.com>
2023-11-15 03:20:58 +0700

    add parkingSpaceName

--- src/app/write_review/[id]/page.tsx
+++ src/app/write_review/[id]/page.tsx
@@ -3,18 +3,21 @@
 import PageTitle from '@/components/common/PageTitle';
 import { NAVBAR_LABEL } from '@/constants/LABEL';
 import { Card, Stack, TextField } from '@mui/material';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ButtonCV2X from '@/components/common/ButtonCV2X';
 import React from 'react';
 import axios from 'axios';
+import { AuthContext } from '@/context/auth-context';
 
 export default function Home({params,}: {
-	params: { parking_space_id: string };
+	params: { id: string };
 }) {
 
     const [message, setMessage] = useState<string>('');
     const [isSubmit, setIsSubmit] = useState<boolean>(false);
-  
+    const [parkingSpaceName, setParkingSpceName] = useState<string>('');
+    const authContext = useContext(AuthContext);
+    console.log(authContext.authState.token)
     const handleSubmit = async () => {
       setIsSubmit(true);
   
@@ -23,9 +26,12 @@ export default function Home({params,}: {
       }
   
       try {
-        const response = await axios.post(`http://localhost:6000/createReview/${params.parking_space_id as string}`, {
+        const response = await axios.post(`http://localhost:6000/createReview/${params.id as string}`, {
+
           message,
-        });
+        },{headers:{
+          Authorization:authContext.authState.token
+        }});
       } catch (error) {
         console.error('Submit failed:', error);
       }
@@ -33,13 +39,28 @@ export default function Home({params,}: {
       setIsSubmit(false);
     };
 
+    useEffect(() => {
+      const getParkingSpaceName = async () => {
+        await axios
+        .get(
+            `http://localhost:4000/getParkingSpace/${params.id as string}`
+        )
+        .then((response) => {
+          setParkingSpceName(response.data["name"])
+        })
+        .catch((error) => {
+            console.error('Error fetching parkingSpaceName:', error);
+        });
+      }
+      getParkingSpaceName();
+    }, []);
+
     return (
         <Stack className='gap-16'>
             <PageTitle title= {"Write "+NAVBAR_LABEL.REVIEW}/>
             <Card className='w-full rounded-lg border-2'>
               <Stack  className='gap-16 p-8'>
-                <p>{"Parking lot's name"}</p>
-                <p>{"Username"}</p>
+                <p>{parkingSpaceName}</p>
                 <TextField
 					        error={isSubmit && message === ''}
 					        placeholder="Write your review here..."
